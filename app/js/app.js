c105f9d6f647c8d60e5b4542ea3653a1abe2b994
IOSMonkey <ysliuwen@126.com>
2014-05-11 23:00:00 +0800

    update

--- app/js/app.js
+++ app/js/app.js
@@ -1,49 +1,21 @@
 'use strict';
 var app = angular.module('todolist', ['ngDragDrop','ngRoute']);
-app.controller('MainCtrl', function ($scope) {
-    $scope.mon = [
-        'John',
-        'Jack',
-        'Mark',
-        'Ernie'
-    ];
-    $scope.tur = [
-        'Jane',
-        'Jill',
-        'Betty',
-        'Mary'
-    ];
-    $scope.wen = [
-        'John',
-        'Jack',
-        'Mark',
-        'Ernie'
-    ];
-    $scope.thu = [
-        'John',
-        'Jack',
-        'Mark',
-        'Ernie'
-    ];
-    $scope.fri = [
-        'John',
-        'Jack',
-        'Mark',
-        'Ernie'
-    ];
-    $scope.sat = [
-        'John',
-        'Jack',
-        'Mark',
-        'Ernie'
-    ];
-    $scope.sun = [
-        'John',
-        'Jack',
-        'Mark',
-        'Ernie'
-    ];
-    $scope.addText = "";
+app.factory('DataServices',function(){
+    var task = [];
+    var goal = [];
+    return {
+        task:task,
+        goal:goal
+    };
+});
+app.controller('MainCtrl', function ($scope,$rootScope,DataServices) {
+    $scope.mon = ['No Task'];
+    $scope.tue = ['No Task'];
+    $scope.wen = DataServices.task;
+    $scope.thu = DataServices.task;
+    $scope.fri = ['No Task'];
+    $scope.sat = ['No Task'];
+    $scope.sun = ['No Task'];
 
     $scope.dropSuccessHandler = function ($event, index, array) {
         array.splice(index, 1);
@@ -54,14 +26,34 @@ app.controller('MainCtrl', function ($scope) {
     };
 
 });
-app.controller('TestCtrl',function($scope){
-
+app.controller('TaskCtrl',function($scope,DataServices){
+    $scope.taskContent = "";
+    $scope.taskTime = "";
+    $scope.goalContent = "";
+    $scope.goalDue = "";
+    $scope.removeTask = function(){
+        window.alert($scope.task);
+    };
+    $scope.addTask = function(){
+        if($scope.goalContent.length == 0 || $scope.taskTime.length == 0) {
+            window.alert('Error');
+            return;
+        }
+        DataServices.task.push($scope.taskContent);
+    };
+    $scope.addGoal = function(){
+        if($scope.goalContent.length == 0 || $scope.goalDue.length == 0) {
+            window.alert('Error');
+            return;
+        }
+        DataServices.task.push($scope.goalContent);
+    };
 });
 app.config(['$routeProvider',function ($routeProvider) {
     $routeProvider
         .when('/edit', {
             templateUrl: 'views/newgoal.html',
-            controller: 'TestCtrl'})
+            controller: 'TaskCtrl'})
         .when('/', {
             templateUrl: 'views/detail.html',
             controller: 'MainCtrl'})
